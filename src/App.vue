578ed4927c08db1bb932be9987f3e0765fb8b4f7
杨家馨 <578755236@qq.com>
2017-08-22 14:35:05 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    Hollo vue
+  </div>
+</template>
+
+<script type="text-ecmascript-6">
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
