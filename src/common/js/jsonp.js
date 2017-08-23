aa47d59d815465efa6cb8c5f0f9cacfc1c6b3e94
杨家馨 <578755236@qq.com>
2017-08-23 17:31:53 +0800

    JSONP封装

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,10 +1,10 @@
 import originJsonp from 'jsonp'
 
-export default function jsonp(url, data, option) {
-  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+export default function jsonp(url, data, option) { // option 回调函数
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data) // 如果小于0 就是？否则就是&
 
   return new Promise((resolve, reject) => {
-    originJsonp(url, option, (err, data) => {
+    originJsonp(url, option, (err, data) => { // option 回调函数两个参数
       if (!err) {
         resolve(data)
       } else {
@@ -14,11 +14,12 @@ export default function jsonp(url, data, option) {
   })
 }
 
+// url的封装
 export function param(data) {
   let url = ''
   for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
-    url += '&' + k + '=' + encodeURIComponent(value)
+    url += '&' + k + '=' + encodeURIComponent(value) // encodeURIComponent把字符串作为URI 组件进行编码
   }
   return url ? url.substring(1) : ''
 }
