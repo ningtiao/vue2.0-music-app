d12486fe6f51bbd2cf522f87c927b487e95bbe10
杨家馨 <578755236@qq.com>
2017-08-24 16:25:43 +0800

    轮播图数据请求

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -12,10 +12,23 @@
 </template>
 
 <script type="text/ecmascript-6">
-  // import {getRecommend} from 'api/recommend'
-  // export default {
+  import {getRecommend} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
 
-  // }
+  export default {
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() { // 轮播图方法
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.slider)
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
