7779c74020f9c19e25d4b4b36136192997b08589
杨家馨 <578755236@qq.com>
2017-08-25 11:51:01 +0800

    轮播图的实现

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -12,10 +19,16 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
     created() {
       this._getRecommend()
     },
@@ -23,10 +36,13 @@
       _getRecommend() { // 轮播图方法
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       }
+    },
+    components: {
+      Slider
     }
   }
 </script>
